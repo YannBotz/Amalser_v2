4ef05b962fa16ab10de27fe505a259793c86e080
JanithaChathuranga <86745601+JanithaChathuranga@users.noreply.github.com>
2021-09-10 11:22:38 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Amalser*:  ```" + msg + "```"
+    return "✅ *Janiya*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Amalser*:  ```" + msg + "```"
+    return "🛑 *Janiya*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Amalser*:  ```" + msg + "```"
+    return "⏺️ *Janiya*:  ```" + msg + "```"
 }
 
 
