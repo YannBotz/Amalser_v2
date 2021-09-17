ac8a856b3c9a95dd589af4ce501bc7d9f9ef3a08
JanithaChathuranga <86745601+JanithaChathuranga@users.noreply.github.com>
2021-09-18 02:17:41 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,31 +1,44 @@
 const Asena = require('../events');
+
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+
 const axios = require('axios');
 
 const Language = require('../language');
+
 const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
     var r_text = new Array ();
+
     
+
     
+
    
+
   r_text[0] = "https://m.imgur.com/a/bV9ednf.jpeg";
+
     
+
     
+
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*creater Janiya*
-owner number wa.me/94703767171
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Janiya*
+
+*owner number wa.me/94703767171*
 
 *bot setting video : https://youtube.com/channel/UCCtyinnnHW_10jom_efwusA*
 
 *githublink : https://github.com/JanithaChathuranga/Amalser_v2*
 
-*Fallow : https://www.instagram.com/janitha129?r=nametag*
-'}) 
+*Fallow Me Insta : https://www.instagram.com/janitha129?r=nametag*
+
+`}) 
 
 }));
+
