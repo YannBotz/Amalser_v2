d1c1890601aed9e1302d758d97f462f9e48885ed
Janiya <86745601+JanithaChathuranga@users.noreply.github.com>
2021-09-27 04:06:29 +0530

    Update janiya.js

--- plugins/janiya.js
+++ plugins/janiya.js
@@ -8,7 +8,7 @@ const Language = require('../language');
 
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'janiya', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
     var r_text = new Array ();
 
