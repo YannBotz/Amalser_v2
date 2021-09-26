25ba4fcbc8d5f0366e26642a8fe0e8132bf4b507
Janiya <86745601+JanithaChathuranga@users.noreply.github.com>
2021-09-27 01:31:59 +0530

    Update random.js

--- plugins/random.js
+++ plugins/random.js
@@ -28,6 +28,6 @@ Asena.addCommand({ pattern: 'random ?(.*)', fromMe: false, desc: 'word image' },
 
     var ttinullimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '```PublicBot```' })
+    await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '👾ᴊᴀɴɪʏᴀ ᴏꜰꜰɪᴄɪᴀʟ ʙᴏᴛ👾' })
 
 }));
