ef4896c3dd268417bad0080b757fb0242921cdda
Janiya <86745601+JanithaChathuranga@users.noreply.github.com>
2021-10-15 17:34:11 +0530

    Update removebg.js

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -88,7 +88,7 @@ else if (Config.WORKTYPE == 'public') {
 		    fs.createWriteStream('rbg.png')
         );
     
-        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.png); //, {filename: 'Janiya.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.png, {filename: 'Janiya.png', mimetype: Mimetype.png});
         await load.delete();
     }));
     
